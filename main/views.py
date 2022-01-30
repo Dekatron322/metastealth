62b78ce42331e1015c3c8e50e0481582f2e87bae
Dekatron322 <62252129+Dekatron322@users.noreply.github.com>
2022-01-30 00:02:36 -0800

    first commit

---
+++ main/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render
+from django.utils import timezone
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse, reverse_lazy
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.models import User
+from .models import *
+from django.core.mail import send_mail
+
+from datetime import datetime
+import datetime as dt
+import requests
+
+#from .forms import UserForm
+
+
+
+
+def RaySendMail(request, subject, message, to_email):
+
+	send_mail(
+	    subject,
+	    message,
+	    'info@oilreach.co.uk',
+	    [to_email,],
+	    fail_silently=False,
+	)
+
+
+
+def IndexView(request):
+	if request.method == "POST":
+		pass
+
+
+	else:
+		context = {}
+		return render(request, "main/index.html", context )
